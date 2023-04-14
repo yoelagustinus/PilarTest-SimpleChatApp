de93834989f527f385067ead9cce8ad37424a0c8
Yoel Agustinus <yoelagustinus7@gmail.com>
2023-04-14 20:51:15 +0700

    update

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const btn_kirim = document.querySelector("#kirim");
+const input = document.querySelector("#input");
+const div_display = document.querySelector(".container-pesan");
+
+const socket = io();
+socket.on("connect", () => console.log("socket connected!"));
+
+const createBubbleChat = chat => {
+    const div_pesan = document.createElement("div");
+    div_pesan.classList.add("pesan");
+    div_pesan.innerHTML = chat;
+    return div_pesan;
+}
+
+btn_kirim.addEventListener("click", () => {
+    const bubbleChat = createBubbleChat(input.value);
+    div_display.appendChild(bubbleChat);
+    socket.emit("kirim-pesan", input.value);
+    input.value = ""; 
+});
+
+socket.on("pesan-baru", pesan=>{
+    const bubbleChat = createBubbleChat(pesan);
+    bubbleChat.classList.add("pesan-r");
+    div_display.appendChild(bubbleChat);
+});
